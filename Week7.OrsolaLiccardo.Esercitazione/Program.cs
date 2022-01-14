4aeaeae429545d9ec72d9e98b6c381807044916b
Orsola Liccardo <orsola.liccardo@avanade.com>
2022-01-14 14:53:04 +0100

    FINITO

--- Week7.OrsolaLiccardo.Esercitazione/Program.cs
+++ Week7.OrsolaLiccardo.Esercitazione/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Week7.OrsolaLiccardo.Esercitazione;
+using Week7.OrsolaLiccardo.Esercitazione.Models;
 
 Console.WriteLine("Hello, World!");
 
 using (var ctx = new Context())
 {
-   
+
 
 }
